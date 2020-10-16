e706db109230ffda50281d4a90899c51486c5fe9
Irwin Felicien <felicienirwin@outlook.com>
2020-10-16 01:58:17 -0500

    Add project files.

---
+++ vending_Machine/Source.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+string myMenu(void);
+bool onNOff();
+int myMoney();
+
+int main() {
+	bool vendingMachine = onNOff();
+	
+
+	cout << vendingMachine << endl;
+}
+
+bool onNOff(void) {
+	bool systemON = true;
+	int power;
+
+	start2:
+
+	cout << "do you want to turn the system on? " << endl;
+	cout << "Press 1 for ON and 2 for Off: ";
+	cin >> power;
+
+	if (power == 1) {
+		cout << "powering sytem on: ";
+		return true;
+	}
+	else if (power == 2){
+		cout << "System is not powered on: ";
+		return false;
+	}
+	else {
+		goto start2;
+	}
+
+}
+
+string myMenu(void) {
+	int myInput;
+	double price;
+	string mySnack;
+
+	start:
+
+	cout << "Select item: " << endl;
+	cout << "1. Coke \t $0.95\n2. Doritos \t 0.75\n3. Snickers \t $0.55\n4. Chex Mix \t $0.60\n5. Pepsi \t $0.90" << endl;
+	cin >> myInput;
+
+	switch (myInput) {
+
+		case 1: mySnack = "Coke";
+			return mySnack;
+			break;
+		case 2: mySnack = "Doritos";
+			return mySnack;
+			break;
+		case 3: mySnack = "Snickers";
+			return mySnack;
+			break;
+		case 4: mySnack = "Chex Mix";
+			return mySnack;
+			break;
+		case 5: mySnack = "Pepsi";
+			return mySnack;
+			break;
+		default: 
+			cout << "Please make a valid selection ";
+			goto start;	
+	}
+}
