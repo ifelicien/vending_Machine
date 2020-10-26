9be5dd105b50b3692c8b44996d0302a443add152
Irwin Felicien <felicienirwin@outlook.com>
2020-10-25 22:11:50 -0500

    linked working functions

--- vending_Machine/Source.cpp
+++ vending_Machine/Source.cpp
@@ -4,18 +4,22 @@ using namespace std;
 
 string myMenu(void);
 bool powerSwitch();
-int myMoney();
-int payment(int);
-int item_Refill();
-int coins_container();
-int item_Container();
-int coin_Return();
-double laborCost(int);
+int turnPowerOn();
+//int item_Refill(void); 
+int coins_container(void); //
+//int item_Container(void); 
+double coin_Return(double, double); 
+double payment(double);
+double laborCost(double);
 double overhead(int);
 double profit_(int price);
-double coin_Refill();
+double coin_Refill(void); //
 
 bool power = false;
+
+const double labor_Percentage = 0.25;
+const double percent_Overhead = 0.05;
+const double item_Cost = 0.35;
 
 class Snack {
 	public:	
@@ -23,11 +27,16 @@ class Snack {
 		string name;
 		double price;
 	Snack(string n, double p) {
-		name = p;
+		name = n;
 		price = p;
 	}
 	string  printself() {
 		cout << inventory << ":" << name << " " << price << endl;
+		inventory--;
+		return "";
+	}
+	void refil(){
+		inventory = 30;
 	}
 };
 
@@ -37,82 +46,124 @@ int main() {
 	Snack Snickers("Snickers", 0.55);
 	Snack ChexMix("Chex Mix", 0.60);
 	Snack Pepsi("Pepsi", 0.90);
-}
-
-int turnPowerOn() {
-	!power ? powerSwitch() : power;  // if no power use powerSwitch function to turn power on
-}
-
-bool powerSwitch(void) {
-	cout << "flipping the power switch. ";
-	power = !power;
+	
+	
+	powerSwitch(); myMenu(); payment();
 }
 
 string myMenu(void) {
 	int myInput;
 	double price;
 	string mySnack;
+	Snack Coke("Coke", 0.95);
+	Snack Doritos("Doritos", 0.75);
+	Snack Snickers("Snickers", 0.55);
+	Snack ChexMix("Chex Mix", 0.60);
+	Snack Pepsi("Pepsi", 0.90);
 
-	start:
+start:
 	cout << "Select item: " << endl;
 	cout << "1. Coke \t $0.95\n2. Doritos \t 0.75\n3. Snickers \t $0.55\n4. Chex Mix \t $0.60\n5. Pepsi \t $0.90" << endl;
 	cin >> myInput;
+	// TODO: payment from user 
 
 	switch (myInput) {
 
-		case 1: mySnack = "Coke";
-			price = 0.95;
-			return mySnack;
-			break;
-		case 2: mySnack = "Doritos";
-			return mySnack;
-			break;
-		case 3: mySnack = "Snickers";
-			return mySnack;
-			break;
-		case 4: mySnack = "Chex Mix";
-			return mySnack;
-			break;
-		case 5: mySnack = "Pepsi";
-			return mySnack;
-			break;
-		default: 
-			cout << "Please make a valid selection ";
-			goto start;	
+	case 1: 
+		mySnack = "Coke";
+		cout << Coke.printself();
+		goto start;
+	case 2: 
+		mySnack = "Doritos";
+		cout << Doritos.printself();
+		break;
+	case 3: 
+		mySnack = "Snickers";
+		cout << Snickers.printself();
+		break;
+	case 4: 
+		mySnack = "Chex Mix";
+		cout << ChexMix.printself();
+		break;
+	case 5: 
+		mySnack = "Pepsi";
+		cout << Pepsi.printself();
+		break;
+	default:
+		cout << "Please make a valid selection ";
+		goto start;
 	}
+
+	return "";
 }
 
-
-int payment(int price) {
-	int pay;
-
-	string dispense = (pay >= price) ? "dispensing item " : "Insert correct amount";
+int turnPowerOn() {
+	!power ? powerSwitch() : power;  // if no power use powerSwitch function to turn power on
 	return 0;
 }
 
-double laborCost(int price) {
-	int labor = price * 0.25;
+bool powerSwitch(void) {
+	cout << "flipping the power switch. \n\n";
+	power = !power;
+	return 0;
+}
+
+double payment(double price) {
+	double paid = 0.0;
+	int pennies, nickles, dimes, quarters, one_Dollar, five_Dollar;
+
+	cout << "Please enter payment " << endl;
+
+	cout << "Pennies: ";
+	cin >> pennies;
+	paid += pennies * 0.01;
+
+	cout << "Nickles:";
+	cin >> nickles;
+	paid += nickles * 0.05;
+
+	cout << "Dimes:";
+	cin >> dimes;
+	paid += dimes * 0.10;
+
+	cout << "Quarters:";
+	cin >> quarters;
+	paid += quarters * 0.25;
+
+	cout << "One Dollar:";
+	cin >> one_Dollar;
+	paid += one_Dollar * 1.00;
+
+	cout << "Five Dollar:";
+	cin >> five_Dollar;
+	paid += five_Dollar * 5.00;
+
+	string dispense = (paid >= price) ? "dispensing item " : "Insert correct amount";
+	return paid;
+}
+
+double laborCost(double price) {
+	int labor = price * labor_Percentage;
 	return labor;
 }
 
 double overhead(int price) {
-	double overH = price * 0.05;
+	double overH = price * percent_Overhead;
 	return overH;
 }
 
 double profit_(int price) {
-	int profit = price * (.35 + .05);
+	int profit = price - (item_Cost + percent_Overhead + labor_Percentage);
 	return profit;
 }
 
-double coinRefill() {
+double coinRefill(void) {
 
+	return 0;
 }
 
-int itemRefill() {
+double coin_Return(double itemPrice, double amountPaid) {
+	double refund = itemPrice - amountPaid;
 
-}
-
-int coin_Return() {
-
+	return refund;
 }
